c3da221126b80a1e05d997318305a8a0df4c14d0
Maksym Pokulytyi <mlightpro@gmail.com>
2022-11-17 16:35:13 +0200

    add logos and chenge normalized.css

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,11 @@
 import s from './Logo.module.css';
+import Logoimage from 'assets/png/logoMob_NotRegister.png';
 
 const Logo = () => {
   return (
     <div>
-      <p className={s.logo}>Logo</p>
+      <img src={Logoimage} alt="logo" className={s.logo} width={44} height={47} />
+      {/* <Logoimg className={s.logo} /> */}
     </div>
   );
 };
