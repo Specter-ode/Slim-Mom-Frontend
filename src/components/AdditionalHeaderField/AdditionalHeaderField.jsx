4f59f3b08f0635e1178d6b69cdf4a37c36039798
Vlad Shchelkunov <specter.ode@gmail.com>
2022-11-25 23:04:54 +0200

    backArrow logic added

--- src/components/AdditionalHeaderField/AdditionalHeaderField.jsx
+++ src/components/AdditionalHeaderField/AdditionalHeaderField.jsx
@@ -1,25 +1,40 @@
 import s from './AdditionalHeaderField.module.css';
 import { ReactComponent as BackArrow } from 'assets/icons/back-arrow.svg';
 import { UserInfo } from 'components';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getModalStatus } from 'redux/auth/auth-selector';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
+import { updateModalStatus } from 'redux/auth/auth-slice';
 // import { getUser, getToken } from 'redux/auth/auth-selector';
 // import { logout } from 'redux/auth/auth-operations';
 const AdditionalHeaderField = () => {
   const showModal = useSelector(getModalStatus);
   const { pathname } = useLocation();
-  // const { name } = useSelector(getUser);
-  // const token = useSelector(getToken);
-
-  const token = true;
+  const dispatch = useDispatch();
+  const isLogin = true;
 
   return (
     <div className={s.block}>
-      {(showModal || pathname === '/diary' || pathname === '/diary-mobile-form') && (
-        <BackArrow className={s.icon} />
+      {showModal && (
+        <button
+          onClick={() => {
+            dispatch(updateModalStatus(true));
+          }}
+        >
+          <BackArrow className={s.icon} />
+        </button>
       )}
-      {token && <UserInfo />}
+      {pathname === '/diary' && (
+        <Navigate to="/calculator">
+          <BackArrow className={s.icon} />
+        </Navigate>
+      )}
+      {pathname === '/diary-mobile-form' && (
+        <Navigate to="/diary">
+          <BackArrow className={s.icon} />
+        </Navigate>
+      )}
+      {isLogin && <UserInfo />}
     </div>
   );
 };
